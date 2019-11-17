7c167dc3ebbf8c44af511a81376ee754fe6c35c1
PeterJanssen <PeterJanssen@users.noreply.github.com>
2019-11-17 20:17:29 +0100

    Add Activities for creating an account or sigining in with an existing account
    These activties are still empty. The layout for the pages are created but these still need to change to reflect the mockups if needed.
    
    Images and resources are bought so are free to use for this project.

---
+++ app/src/main/java/be/pxl/mobdev2019/cityWatch/account_activities/CreateAccountActivity.kt
@@ -0,0 +1,13 @@
+package be.pxl.mobdev2019.cityWatch.account_activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import be.pxl.mobdev2019.cityWatch.R
+
+class CreateAccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
