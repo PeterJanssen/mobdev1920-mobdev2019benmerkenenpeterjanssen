991628dfb7ea14ed39a707247694ceb2063edf91
PeterJanssen <PeterJanssen@users.noreply.github.com>
2019-12-30 11:20:43 +0100

    Update MainActivity.kt
    Added onSupportNavigateUp override to implement up button for navigation

--- app/src/main/java/be/pxl/mobdev2019/cityWatch/MainActivity.kt
+++ app/src/main/java/be/pxl/mobdev2019/cityWatch/MainActivity.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val appBarConfiguration: AppBarConfiguration = AppBarConfiguration(
+        setOf(
+            R.id.navigation_lists,
+            R.id.navigation_explore,
+            R.id.navigation_see_all_posts,
+            R.id.navigation_account
+        )
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,15 +25,11 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_lists,
-                R.id.navigation_explore,
-                R.id.navigation_see_all_posts,
-                R.id.navigation_account
-            )
-        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
     }
 }
