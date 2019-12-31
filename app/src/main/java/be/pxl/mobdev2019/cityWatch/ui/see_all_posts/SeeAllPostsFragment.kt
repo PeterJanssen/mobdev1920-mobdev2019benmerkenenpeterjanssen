5b194360d74e5add97a125fd5b51962f138ce07f
PeterJanssen <PeterJanssen@users.noreply.github.com>
2019-12-31 15:54:10 +0100

    Add basic implementation of google maps
    - Add an empty google maps fragment

--- app/src/main/java/be/pxl/mobdev2019/cityWatch/ui/see_all_posts/SeeAllPostsFragment.kt
+++ app/src/main/java/be/pxl/mobdev2019/cityWatch/ui/see_all_posts/SeeAllPostsFragment.kt
@@ -6,27 +6,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import be.pxl.mobdev2019.cityWatch.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import kotlinx.android.synthetic.main.fragment_see_all_posts.*
 
-class SeeAllPostsFragment : Fragment() {
+class SeeAllPostsFragment : Fragment(), OnMapReadyCallback {
 
-    private lateinit var notificationsViewModel: SeeAllPostsViewModel
+    private lateinit var seeAllPostsViewModel: SeeAllPostsViewModel
+    private lateinit var googleMap: GoogleMap
+
+    override fun onMapReady(map: GoogleMap?) {
+        map?.let {
+            googleMap = it
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProviders.of(this).get(SeeAllPostsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_see_all_posts, container, false)
-        val textView: TextView = root.findViewById(R.id.text_see_all_posts)
-        notificationsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
-        return root
+        val view: View = inflater.inflate(R.layout.fragment_see_all_posts, container, false)
+        val mMapView = view.findViewById<MapView>(R.id.map_view)
+        mMapView.onCreate(savedInstanceState)
+        mMapView.getMapAsync(this)
+        mMapView.onResume()
+        return view
     }
+
+
 }
