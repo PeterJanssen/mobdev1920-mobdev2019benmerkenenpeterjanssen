c5a7cb873544ac07d9eb54d5856cd7e2ba73cb6e
PeterJanssen <PeterJanssen@users.noreply.github.com>
2020-01-04 21:26:03 +0100

    Add extra buttons to map, menu for changing map type and dialogue
    Added a menu to the map fragment for changing the map type to satellite, hybrid, etc.
    Added a compass when the map isn't pointing to the north
    Added a dialogue to turn on location if it is not already turned on

--- app/src/main/java/be/pxl/mobdev2019/cityWatch/ui/see_all_reports_on_map/SeeAllReportsOnMapFragment.kt
+++ app/src/main/java/be/pxl/mobdev2019/cityWatch/ui/see_all_reports_on_map/SeeAllReportsOnMapFragment.kt
@@ -2,18 +2,25 @@ package be.pxl.mobdev2019.cityWatch.ui.see_all_reports_on_map
 
 
 import android.Manifest
+import android.app.Activity
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.content.IntentSender.SendIntentException
 import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import be.pxl.mobdev2019.cityWatch.R
 import be.pxl.mobdev2019.cityWatch.util.toast
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
+
 
 class SeeAllReportsOnMapFragment : Fragment(), OnMapReadyCallback {
 
@@ -31,6 +38,8 @@ class SeeAllReportsOnMapFragment : Fragment(), OnMapReadyCallback {
         )
 
         if (permission == PackageManager.PERMISSION_GRANTED) {
+            googleMap.isMyLocationEnabled = true
+            getCurrentLocation()
             addButtonsToMap()
         } else {
             requestPermissions(
@@ -41,6 +50,58 @@ class SeeAllReportsOnMapFragment : Fragment(), OnMapReadyCallback {
             )
         }
 
+    }
+
+    private fun getCurrentLocation() {
+        val locationRequest = LocationRequest.create()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+
+        val result =
+            LocationServices.getSettingsClient(requireActivity())
+                .checkLocationSettings(builder.build())
+
+        result.addOnCompleteListener { task ->
+            try {
+                val response: LocationSettingsResponse? =
+                    task.getResult(ApiException::class.java)
+                // All location settings are satisfied. The client can initialize location
+                // requests here.
+            } catch (exception: ApiException) {
+                when (exception.statusCode) {
+                    LocationSettingsStatusCodes.RESOLUTION_REQUIRED ->  // Location settings are not satisfied. But could be fixed by showing the
+                        // user a dialog.
+                        try { // Cast to a resolvable exception.
+                            val resolvable =
+                                exception as ResolvableApiException
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            resolvable.startResolutionForResult(
+                                activity,
+                                LocationRequest.PRIORITY_HIGH_ACCURACY
+                            )
+                        } catch (e: SendIntentException) { // Ignore the error.
+                        } catch (e: ClassCastException) { // Ignore, should be an impossible error.
+                        }
+                    LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            LocationRequest.PRIORITY_HIGH_ACCURACY -> when (resultCode) {
+                Activity.RESULT_OK ->  // All required changes were successfully made
+                    Log.i(TAG, "onActivityResult: GPS Enabled by user")
+                Activity.RESULT_CANCELED ->  // The user was asked to change settings, but chose not to
+                    Log.i(TAG, "onActivityResult: User rejected GPS request")
+                else -> {
+                }
+            }
+        }
     }
 
     override fun onRequestPermissionsResult(
@@ -63,18 +124,44 @@ class SeeAllReportsOnMapFragment : Fragment(), OnMapReadyCallback {
 
     private fun addButtonsToMap() {
         val mapSettings = googleMap.uiSettings
-        googleMap.isMyLocationEnabled = true
-        mapSettings?.isCompassEnabled = true
         mapSettings?.isMyLocationButtonEnabled = true
+        mapSettings?.isCompassEnabled = true
         mapSettings?.isZoomControlsEnabled = true
     }
 
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.map_options, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        // Change the map type based on the user's selection.
+        R.id.normal_map -> {
+            googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+            true
+        }
+        R.id.hybrid_map -> {
+            googleMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+            true
+        }
+        R.id.satellite_map -> {
+            googleMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            true
+        }
+        R.id.terrain_map -> {
+            googleMap.mapType = GoogleMap.MAP_TYPE_TERRAIN
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         val view: View = inflater.inflate(R.layout.fragment_see_all_posts_on_map, container, false)
         val mMapView = view.findViewById<MapView>(R.id.map_view)
         mMapView.onCreate(savedInstanceState)
